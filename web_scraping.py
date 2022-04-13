0686198b4cab25d8fbe54bf0863b6120318e9bd2
Ayushi Jain <73245895+ayushi2019031@users.noreply.github.com>
2022-04-13 22:48:50 +0530

    Updated python file for storing of course information

--- web_scraping.py
+++ web_scraping.py
@@ -16,6 +16,7 @@ PATH = './course_data/'
 from os import listdir, remove
 from os.path import isfile, join
 import pandas as pd
+import json
 
 xlsx_files = []
 docx_files = []
@@ -34,10 +35,36 @@ for file_ in listdir(PATH):
 import openpyxl
 import string
 
+def getcoursedesc(course_desc, wb_obj ,sheet):
+	string1 = chr(ord(course_desc[0]) + 1);
+	num1 = int(course_desc[1:])
+	string = string1 + str(num1)
+	print(string)
+	return sheet[string].value
+
+def gettopics(topic, wb_obj, sheet):
+	string = ""
+	f = True
+	list_of_topics = []
+	while f:
+		string1 = topic[0];
+		string2 = int(topic[1:]) + 1; 
+		string = string1 + str(string2)
+		topic = string
+		if (sheet[string].value != None):
+			list_of_topics.append(sheet[string].value)
+		else:
+			f  = False;
+	return list_of_topics
+
+
+dictionary_course_name = {}
+
 for i in range(len(xlsx_files)):
     print(xlsx_files[i])
+    # break
     wb_obj = openpyxl.load_workbook(join(PATH, xlsx_files[i]), read_only=True, data_only=True) 
-
+    sheet  = wb_obj.active
     course_desc = ""
     # pre_req = ""
     topic = ""
@@ -53,13 +80,28 @@ for i in range(len(xlsx_files)):
 
             if cell_value.lower() == "course description":
                 course_desc = cell_address
+                actual_course_desc = getcoursedesc(course_desc, wb_obj, sheet)
+                print(actual_course_desc)
             # elif cell_value.lower() == "pre-requisites":
             #     pre_req = cell_address
             elif cell_value.lower() == "lecture topic" or cell_value.lower() == "topics":
                 topic = cell_address
+                actual_topics = gettopics(topic, wb_obj, sheet)
+                print(actual_topics)
             elif cell_value.lower() == "assessment plan":
                 assessment_plan = cell_address
+
+    dictionary_xx = {}
+    dictionary_xx["course_desc"] = actual_course_desc
+    dictionary_xx["lecture topic"] = actual_topics
+    dictionary_xx["assessment plan"] = assessment_plan
+
+    dictionary_course_name[xlsx_files[i]] = dictionary_xx
+    # break
 
     # if not course_desc or not topic or not assessment_plan:
     #     print(course_desc, topic, assessment_plan)
     #     print(xlsx_files[i])
+import json
+with open("course_details.json", "w") as outfile:
+    json.dump(dictionary_course_name, outfile)
